9bcafc8b6e31a75316231a2f83200f0fbbdc08f4
t1mlin <tim.lin@crypto.com>
2022-08-01 16:39:45 +0800

    :tada:  init projects

---
+++ contracts/ChequeBank.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+contract ChequeBank {
+    struct ChequeInfo {
+        uint amount;
+        bytes32 chequeId;
+        uint32 validFrom;
+        uint32 validThru;
+        address payee;
+        address payer;
+    }
+    struct SignOverInfo {
+        uint8 counter;
+        bytes32 chequeId;
+        address oldPayee;
+        address newPayee;
+    }
+
+    struct Cheque {
+        ChequeInfo chequeInfo;
+        bytes sig;
+    }
+    struct SignOver {
+        SignOverInfo signOverInfo;
+        bytes sig;
+    }
+
+    function deposit() external payable {}
+
+    function withdraw(uint amount) external {}
+
+    function withdrawTo(uint amount, address payable recipient) external {}
+
+    function redeem(Cheque memory chequeData) external {}
+
+    function revoke(bytes32 chequeId) external {}
+
+    function notifySignOver(SignOver memory signOverData) external {}
+
+    function redeemSignOver(
+        Cheque memory chequeData,
+        SignOver[] memory signOverData
+    ) external {}
+
+    function isChequeValid(
+        address payee,
+        Cheque memory chequeData,
+        SignOver[] memory signOverData
+    ) public view returns (bool) {}
+}
